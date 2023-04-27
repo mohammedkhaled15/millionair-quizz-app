6ec78fffac0e181d2f2d05f4ce632220074aff56
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-04-27 13:08:03 +0200

    15 quest

--- src/data.js
+++ src/data.js
@@ -65,6 +65,270 @@ export const data = [
       },
     ],
   },
+  {
+    id: 4,
+    question: "In which Italian city can you find the Colosseum?",
+    answers: [
+      {
+        text: "Venice",
+        correct: false,
+      },
+      {
+        text: "Rome",
+        correct: true,
+      },
+      {
+        text: "Naples",
+        correct: false,
+      },
+      {
+        text: "Milan",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Who was the 40th President of the United States?",
+    answers: [
+      {
+        text: "Franklin D. Roosevelt",
+        correct: false,
+      },
+      {
+        text: "Ronald Reagan",
+        correct: true,
+      },
+      {
+        text: "Bill Clinton",
+        correct: false,
+      },
+      {
+        text: "George W. Bush",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "What is the largest active volcano in the world?",
+    answers: [
+      {
+        text: "Mount Etna",
+        correct: false,
+      },
+      {
+        text: "Mount Vesuvius",
+        correct: false,
+      },
+      {
+        text: "Mouna Loa",
+        correct: true,
+      },
+      {
+        text: "Mount Batur",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "When did Lenin die?",
+    answers: [
+      {
+        text: "21 January 1924",
+        correct: true,
+      },
+      {
+        text: "21 January 1934",
+        correct: false,
+      },
+      {
+        text: "21 January 1944",
+        correct: false,
+      },
+      {
+        text: "21 January 1954",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "In which museum can you find Leonardo Da Vinci’s Mona Lisa?",
+    answers: [
+      {
+        text: "Le Louvre",
+        correct: true,
+      },
+      {
+        text: "Uffizi Museum",
+        correct: false,
+      },
+      {
+        text: "British Museum",
+        correct: false,
+      },
+      {
+        text: "Metropolitan Museum of Art",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "When did Salt Lake City host the Winter Olympics?",
+    answers: [
+      {
+        text: "1992",
+        correct: false,
+      },
+      {
+        text: "1998",
+        correct: false,
+      },
+      {
+        text: "2002",
+        correct: true,
+      },
+      {
+        text: "2008",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "When did Hitler invade Poland?",
+    answers: [
+      {
+        text: "1st September 1939",
+        correct: true,
+      },
+      {
+        text: "11th November 1939",
+        correct: false,
+      },
+      {
+        text: "8th May 1940",
+        correct: false,
+      },
+      {
+        text: "1st December 1940",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "Who was George W. Bush’ vice president?",
+    answers: [
+      {
+        text: "Kamala Harris",
+        correct: false,
+      },
+      {
+        text: "Joe Biden",
+        correct: false,
+      },
+      {
+        text: "Dick Cheney",
+        correct: true,
+      },
+      {
+        text: "Al Gore",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "Which famous inventor invented the telephone?",
+    answers: [
+      {
+        text: "Thomas Edison",
+        correct: false,
+      },
+      {
+        text: "Benjamin Franklin",
+        correct: false,
+      },
+      {
+        text: "Alexander Graham Bell",
+        correct: true,
+      },
+      {
+        text: "Nikola Tesla",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "What does the Richter scale measure?",
+    answers: [
+      {
+        text: "Wind Speed",
+        correct: false,
+      },
+      {
+        text: "Temperature",
+        correct: false,
+      },
+      {
+        text: "Tornado Strength",
+        correct: false,
+      },
+      {
+        text: "Earthquake intensity",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "Which country is the footballer Cristiano Ronaldo from?",
+    answers: [
+      {
+        text: "Spain",
+        correct: false,
+      },
+      {
+        text: "Brazil",
+        correct: false,
+      },
+      {
+        text: "Uruguay",
+        correct: false,
+      },
+      {
+        text: "Portugal",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "What is the longest river in the world?",
+    answers: [
+      {
+        text: "Amazon River",
+        correct: false,
+      },
+      {
+        text: "Nile",
+        correct: true,
+      },
+      {
+        text: "Yellow River",
+        correct: false,
+      },
+      {
+        text: "Congo River",
+        correct: false,
+      },
+    ],
+  },
 ];
 
 export const moneyData = [
