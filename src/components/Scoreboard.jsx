351593307d37a2da8cc8933b535cff6133366491
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-10 17:21:40 +0300

    serializing data

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -6,13 +6,14 @@ import { DataGrid } from '@mui/x-data-grid';
 const Scoreboard = ({ setShowScoreboard }) => {
 
   const [userData, setUserData] = useState([])
-  const [rowSerial, setRowSerial] = useState(0)
 
   useEffect(() => {
     const getAllUsers = async () => {
       try {
+        let sn = 0
         const res = await privateRequest.get("/")
-        setUserData(res.data)
+        const newData = res.data.map(user => { return { sn: ++sn, ...user } })
+        setUserData(newData)
       } catch (error) {
         console.log(error)
       }
@@ -20,26 +21,8 @@ const Scoreboard = ({ setShowScoreboard }) => {
     getAllUsers()
   }, [])
 
-  let count = 0
-
   const columns = [
-    {
-      field: '#',
-      headerName: '#',
-      width: 50,
-      // renderCell: (params) => params.api.getRowIndexRelativeToVisibleRows(params.id) + 1
-      // renderCell: params => {
-      //   // console.log(params)
-      //   let noOfRows = params.api.getRowsCount()
-      //   // console.log(noOfRows)
-      //   if (count < noOfRows) {
-      //     count++
-      //     console.log(count)
-      //     return count
-      //   }
-      // }
-      renderCell: (params) => params.api.getRowsCount() - count++
-    },
+    { field: 'sn', headerName: '#', width: 50, },
     { field: 'username', headerName: 'Player Name', width: 250 },
     { field: 'topScore', headerName: 'Top Score', width: 100 },
     { field: 'updatedAt', headerName: 'Date Of Top Score', width: 250, renderCell: (params) => params.row.updatedAt.slice(0, 10) },
@@ -49,7 +32,6 @@ const Scoreboard = ({ setShowScoreboard }) => {
   return (
     <div className='relative w-full h-screen flex flex-col items-center '>
       <button className={`rounded-xl bg-[#02274d] p-6 text-xl  font-medium active:bg-[#0a1a2b] absolute top-10 right-10`} onClick={() => setShowScoreboard(false)}>Back To The Game</button>
-
       <div className="mt-32 shadow-md sm:rounded-lg h-[500px] w-3/4">
         <DataGrid
           autoPageSize
@@ -59,7 +41,6 @@ const Scoreboard = ({ setShowScoreboard }) => {
           rows={userData}
           columns={columns}
           getRowId={(row) => row?.username} />
-
       </div>
     </div>
   )
